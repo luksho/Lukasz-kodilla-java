25a1179f3e423238b44488ccb00ab94f9029d38a
Your Name <you@example.com>
2018-02-12 16:06:45 +0100

    update

--- kodilla_testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla_testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+        Calculator calculator = new Calculator();
+        int AddResult = calculator.add(10, 5);
+        int SubtractResult = calculator.subtract(10, 5);
+        if (AddResult == 15 && SubtractResult == 5) {
+            System.out.println("Calculator  OK");
+        } else {
+            System.out.println("Calculator Error!");
+        }
+
     }
 }

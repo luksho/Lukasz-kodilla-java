6b65ef6a3e51bbe2f714d589f875110a0d026bca
Your Name <you@example.com>
2018-02-23 12:31:11 +0100

    Task 7.2

--- kodilla_stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla_stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,15 +1,25 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
     public static void main(String[] args) {
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+
+       /* PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         poemBeautifier.beautify("Text", String::toUpperCase);
         poemBeautifier.beautify("Text", String::toLowerCase);
         poemBeautifier.beautify("Text", text -> "ABC " + text + " ABC");
-        poemBeautifier.beautify("Text", text -> text.replaceAll(text,"BBBB"));
+        poemBeautifier.beautify("Text", text -> text.replaceAll(text, "BBBB"));
     }
-}
 
+    */
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
+    }
+
+}
